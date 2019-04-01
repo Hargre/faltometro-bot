3c76ab292a4c256cdd97b235f9ab6262f813e7e9
Felipe Hargreaves <fhargreaves00@gmail.com>
2019-03-31 22:19:29 -0300

    Update postgres config.

--- models/base.py
+++ models/base.py
@@ -1,5 +1,6 @@
 import os
 from os.path import abspath
+from urllib.parse import urlparse
 
 from peewee import Model
 from peewee import PostgresqlDatabase
@@ -8,7 +9,8 @@ from peewee import SqliteDatabase
 db =  None
 
 if os.getenv('ENV_MODE') == 'PROD':
-    db = PostgresqlDatabase('botdb', host=os.getenv('DATABASE_URL'))
+    url = urlparse(os.getenv('DATABASE_URL'))
+    db = PostgresqlDatabase(url.path[1:], user=url.username, host=url.hostname, password=url.password, port=url.port)
 else:
     db_path = abspath('../bot.db')
     db = SqliteDatabase(db_path)
